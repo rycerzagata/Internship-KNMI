f676a9f9dbc62922e879490b3625a08e12732a94
rycerzagata <aszakiewicz@gmail.com>
2021-06-30 10:35:52 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,3 @@
-
-
 import os
 os.environ['PROJ_LIB'] = 'C:/Users/HP/anaconda3/envs/knmi/Library/share/proj'
 os.environ['GDAL_DATA'] = 'C:/Users/HP/anaconda3/envs/knmi/Library/share/gdal'
@@ -24,14 +22,49 @@ numpy.seterr(divide='ignore', invalid='ignore')
 
 # Calculate NDVI
 ndvi = (band_nir.astype(float) - band_red.astype(float)) / (band_nir + band_red)
+#ndvi[ndvi > 1] = float('NaN')
+#ndvi[ndvi < -1] = float('NaN')
+ndvi[ndvi > 1] = 1
+ndvi[ndvi < -1] = -1
+ndvi[numpy.isnan(ndvi)] = float('NaN')
+ndvi[numpy.isinf(ndvi)] = float('NaN')
+#print(numpy.amax(ndvi), numpy.amin(ndvi))
+show(ndvi)
+
+from xrspatial import convolution
+from xrspatial import focal
+import xarray
+ndvi_arr = xarray.DataArray(ndvi)
+cellsize_x, cellsize_y = convolution.calc_cellsize(ndvi_arr)
+
+# Use an annulus kernel with a ring at a distance from 25-30 cells away from focal point
+outer_radius = cellsize_x * 10
+inner_radius = cellsize_x * 1
+kernel = convolution.circle_kernel(cellsize_x, cellsize_y, outer_radius)
+ndvi_focal = focal.apply(ndvi_arr, kernel)
+#show(ndvi_focal)
+
+veg_map = ndvi_focal.copy()
+veg_map.values[veg_map.values > 0.22] = 1
+veg_map.values[veg_map.values <= 0.22] = 0
+
+import gdal
+Image = gdal.Open('SomeImageName.tif', 1)  # open image in read-write mode
+Band = Image.GetRasterBand(1)
+gdal.SieveFilter(srcBand=Band, maskBand=None, dstBand=Band, threshold=100, connectedness=8, callback=gdal.TermProgress_nocb)
+del Image, Band  # close the datasets.
 
 # Set spatial characteristics of the output object to mirror the input
-kwargs = src.meta
-kwargs.update(
-    dtype=rasterio.float32,
-    count = 1)
+#kwargs = src.meta
+#kwargs.update(
+    #dtype=rasterio.float32,
+    #count = 1)
 
 # Create the file
-with rasterio.open('C:/Users/HP/Documents/Internship/Data/Outputs/ndvi.tif', 'w', **kwargs) as dst:
-        dst.write_band(1, ndvi.astype(rasterio.float32))
+#with rasterio.open('C:/Users/HP/Documents/Internship/Data/Outputs/ndvi.tif', 'w', **kwargs) as dst:
+        #dst.write_band(1, ndvi.astype(rasterio.float32))
+
+
+fp = r"C:/Users/HP/Documents/Internship/Data/dsm_voorschoten_RD_clip.tif"
+dsm = rasterio.open(fp)
 
