bba8074857c12244d78f5fc5f89e48346627d4d3
rycerzagata <aszakiewicz@gmail.com>
2021-08-02 15:48:00 +0200

    Update max_elevation_values.py

--- max_elevation_values.py
+++ max_elevation_values.py
@@ -8,13 +8,18 @@ import numpy as np
 import gdal
 
 # Import a cropped DSM as a numpy array
-fp = r"C:/Users/HP/Documents/Internship/Data/dsm_voorschoten_RD_clip.tif"
+fp = r"dsm_voorschoten_RD_clip.tif"
 ds = gdal.Open(fp)
 myarray = np.array(ds.GetRasterBand(1).ReadAsArray())
-print(myarray.shape)
+height, width = myarray.shape
+
+test_origin = (643, 652)
+rotation_step = 1
 
 
-def rotate(origin, point, angle):
+
+def rotate(origin: (float, float), point: (float, float), angle: int):
+    import math
     """
     Rotate a point counterclockwise by a given angle around a given origin.
 
@@ -33,11 +38,10 @@ import numpy as np
 import math
 
 
-def interpolate(height_map: np.arrray, x: float, y: float) -> float:
+def interpolate(height_map: np.array, point: (float, float)) -> float:
     """
 
-    :param y:
-    :param x:
+    :param point:
     :param height_map: 2D Numpy array of floating point values representing height readings
     :return: Bi-linear interpolation from height_map at a given point
     """
@@ -48,6 +52,7 @@ def interpolate(height_map: np.arrray, x: float, y: float) -> float:
     # ....
     # if condition returns False, AssertionError is raised:
     # assert x == "goodbye", "x should be 'hello'"
+    x, y = point
 
     # Interpolate values
     z00 = height_map[math.floor(x), math.floor(y)]
@@ -58,37 +63,48 @@ def interpolate(height_map: np.arrray, x: float, y: float) -> float:
     return z00 * (1 - x) * (1 - y) + z10 * x * (1 - y) + z01 * (1 - x) * y + z11 * x * y
 
 
-def get_deltas_from_rotation(origin: (float, float), point: (float, float), angle: int) -> (float, float):
-    # TODO Document and implement, now it returns mock values
-    qx, qy = rotate(origin, point, angle)
+def get_points_from_rotation(origin: (float, float), angle: int, num_of_samples: float) -> np.array:
+    # na podstawie kata obrotu w stopniach i ilości sampli zrob liste z pozycjami punktów
+    height, width = myarray.shape
+    step = height // 2 // num_of_samples
     ox, oy = origin
-    dx = qx - ox
-    dy = qy - oy
+    list_of_points = np.zeros((num_of_samples, 2), dtype=tuple)
+    for i in range(num_of_samples):
+        base_point = ox, oy + step * i
+        rotated_point = rotate(origin, base_point, angle)
+        list_of_points[i, 0] = base_point
+        list_of_points[i, 1] = rotated_point
 
-    return dx, dy
+    return list_of_points
 
 
-def scan_environment(height_map: np.array, origin: (float, float), sampling_step: float) -> np.array:
+def scan_environment(height_map: np.array, origin: (float, float), num_of_samples: float) -> np.array:
     """
 
-    :param sampling_step: Sampling advances by this amount on a line from origin to the edge of height_map
+    :param num_of_samples: Number of samples taken on a line from origin to the edge of height_map
     :param height_map: 2D Numpy array of floating point values representing height readings
     :param origin: Point which serves as an anchor for the rotation
     :return: 360 x sampling step array where each row represent a full sampling done on each rotation
     """
 
-    # TODO Test if it works
+    # TODO Change sampling_step to num_step
 
-    samples = np.zeros((360, sampling_step), dtype='float')
+    samples = np.zeros((360, num_of_samples), dtype='float')
     for rotation in range(360):
-        x, y = origin
+        # x, y = origin
         sample_index = 0
+        map_height, map_width = height_map.shape
         # Update deltas according to current rotation
-        delta_x, delta_y = get_deltas_from_rotation(rotation)
-        while 0 < x <= len(height_map) and 0 < y <= len(height_map[x]):
-            x += delta_x
-            y += delta_y
-            samples[rotation, sample_index] = interpolate(height_map, x, y)
+        all_points = get_points_from_rotation(origin, rotation, num_of_samples)
+        rotated_points = all_points[:, 1]
+        for point in rotated_points:
+           # x, y = point
+           # while 0 < x <= map_width and 0 < y <= map_height:
+            samples[rotation, sample_index] = interpolate(height_map, point)
             sample_index += 1
-
     return samples
+
+
+
+
+test_samples = scan_environment(myarray, test_origin, 10)
